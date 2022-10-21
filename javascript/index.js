8f7c58315f588a9316b273eca162c976825166cc
Foullani fatima zohra <foullanifatimazohra@gmail.com>
2022-10-21 11:36:01 -0700

    adding prettier

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,12 @@
+const humberger_menu = document.getElementById('humberger_menu')
 
+humberger_menu.addEventListener('click', () => {
+  let nav = document.getElementById('nav')
 
-const humberger_menu = document.getElementById("humberger_menu");
-
-humberger_menu.addEventListener("click", ()=>{
-    let nav = document.getElementById("nav");
-
-    if(nav.classList.contains("none")){
-        nav.classList.remove("none");
-        nav.classList.add("block");
-    }
-    else{
-        nav.classList.add("none");
-    }
-    
-   
-}
-   
-);
-
+  if (nav.classList.contains('none')) {
+    nav.classList.remove('none')
+    nav.classList.add('block')
+  } else {
+    nav.classList.add('none')
+  }
+})
